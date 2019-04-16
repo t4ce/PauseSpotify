fd33c375eff321611986a4ef71867a3ab641c69b
jonas <jonasb@post.com>
2019-04-16 02:51:11 +0200

    fine tune

--- app/src/main/java/epx/pub/pausespotify/MainActivity.java
+++ app/src/main/java/epx/pub/pausespotify/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.res.ResourcesCompat;
@@ -13,7 +12,6 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 
